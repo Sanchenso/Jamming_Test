c15e4e03fbfe2d7f5e941a9d272041c920b8f673
sanchenso <sanekspbproff@gmail.com>
2023-01-12 22:35:21 +0300

    Add a folder Results, including files: 1. data_jemming_all.csv Generated from the 'detect_jemming_test.py' Including information about detect jamming time 2. data_propuski.csv Generated from the 'detect_jemming_test.py' Including information about length of time and missing packet. 3. newNameDrones.txt Generated from the 'detect_jemming_all.py' If you chose y, renamed all bin files in parent folder (where is the script).

--- detect_jemming_all.py
+++ detect_jemming_all.py
@@ -2,21 +2,80 @@ import subprocess
 import os
 import sys
 import time
+
 path = os.listdir()
 arg2 = sys.argv[1]
 arg3 = sys.argv[2]
-for k in path:
-    if k[-4:] == '.obs' or k[-4:] == '.ubx' or k[-4:] == '.txt':
+
+folder_name = 'Results'  # Папка для вывода результатов
+if not os.path.exists(folder_name):
+    os.mkdir(folder_name)
+
+for k in os.listdir(os.path.join(os.getcwd(), folder_name)): #  зачищаем папку перед началом
+    os.remove(os.path.join(os.getcwd(), folder_name, k))
+
+for k in path: # удаляем лишнее
+    if k[-4:] == '.obs' or k[-4:] == '.ubx':
         print(k)
         os.remove(k)
+path = os.listdir()
+def check():
+    while True:
+        print('please entry y on n')
+        iin = str(input())
+        if iin == 'y' or iin == 'n':
+            return iin
 
-for i in path:
-    if i[-4:] == '.bin':
-        binFile = i[:-4]
-        print(binFile)
-        subprocess.call("python3 detect_jemming_test.py " + binFile + " " + arg2 + " " + arg3, shell=True)
-time.sleep(3)
-for l in path:
-    if l[-4:] == '.obs' or l[-4:] == '.ubx' or l[-4:] == '.txt':
-        print(l)
-        os.remove(l)
+
+print('Do you need to change number of drones?')
+iin = check()
+
+numberLog = 0
+allNumberLog = 0
+
+for logs in path:
+    if logs[-4:] == '.bin':
+        allNumberLog += 1
+
+if iin == 'n':
+    start = time.time()
+    '''Если не нужно переименовывать файлы'''
+    for i in path:
+        if i[-4:] == '.bin':
+            numberLog += 1
+            print(i)
+            print(numberLog, 'of', allNumberLog)
+            subprocess.call("python3 detect_jemming_test.py " + i + " " + arg2 + " " + arg3, shell=True)
+
+elif iin == 'y':
+    print('You sure? This is inversible, please make copy of logs')
+    print('Renumber now?')
+    iin = check()
+    start = time.time()
+    if iin == 'y':
+        '''Если нужно переименовывать файлы'''
+        oldName = {}
+        path = os.listdir()
+        for i in path:
+            if i[-4:] == '.bin':
+                os.rename(i, '1_' + i)
+                oldName['1_' + i] = None
+        with open(os.path.join(os.getcwd(), folder_name, 'newNameDrones.txt'), 'w') as f:
+            for j in sorted(oldName.keys()):
+                numberLog += 1
+                newName = str(numberLog) + '.bin'
+                oldName[j] = newName
+                f.write(str(newName))
+                f.write(' ')
+                f.write(str(j)[2:])
+                f.write('\n')
+                os.rename(j, newName)
+                print(newName)
+                print(numberLog, 'of', allNumberLog)
+                subprocess.call("python3 detect_jemming_test.py " + newName + " " + arg2 + " " + arg3, shell=True)
+            f.close()
+        print('pls check the file: newNameDrones.txt')
+    else:
+        print('repeat a function call')
+    print('Working time:', end=' ')
+print(time.time() - start, end=' sec')
--- detect_jemming_test.py
+++ detect_jemming_test.py
@@ -3,64 +3,87 @@ import os
 import sys
 import subprocess
 from matplotlib.pyplot import figure, show, savefig
+from numpy import timedelta64
 
 ax = figure().gca()
 
-arg = sys.argv[1]												 # номер дрона
+arg = sys.argv[1]  # наименование лога дрона
+arg2 = sys.argv[2]  # значение SNR
+arg3 = sys.argv[3]  # значение кол-ва спутников
+
+if arg[-4:] == ".bin" or arg[-4:] == ".ubx":
+    index = arg.index('.')
+    arg = arg[:index]
 
 binFile = arg + ".bin"
 ubxFile = arg + ".ubx"
 obsFile = arg + ".obs"
 
-subprocess.call("./parser --raw_data " + binFile, shell=True)
+folder_name = 'Results' # Папка для вывода результатов
+if not os.path.exists(folder_name):
+    os.mkdir(folder_name)
 
-os.remove(arg + ".txt")											# удаляем текстовый файл с параметрами автопилота
-os.remove(arg + ".gscl")
-#os.remove(arg + "_channel_123.dat")
-os.rename(arg + "_channel_126.dat", ubxFile)
-
-arg2 = sys.argv[2]												# значение SNR
-arg3 = sys.argv[3]												# значение кол-ва спутников
+'''skip parser if only UBX'''
+try:
+    subprocess.call("./parser --raw_data " + binFile, shell=True)
+    os.remove(arg + ".txt")  # удаляем текстовый файл с параметрами автопилота
+    os.remove(arg + ".gscl")
+    os.remove(arg + "_channel_123.dat")
+except Exception:
+    pass
+try:
+    os.rename(arg + "_channel_126.dat", ubxFile)
+except Exception:
+    pass
 
 subprocess.call("convbin " + ubxFile + " -o " + obsFile + " -os -r ubx", shell=True)
+hdr = gr.rinexheader(obsFile)
+# print(hdr.get('fields')) # вывод типа данных нав. систем
 
-obsG = gr.load(obsFile, use=['G'])	
+obsG = gr.load(obsFile, use=['G'])
 obsR = gr.load(obsFile, use=['R'])
-b = obsG['S1'].time.values.size 				    			# определяем длину временного отрезок
 
-#spisok_name_satG = list((obsG['S1'].sv.values))				# формируем список всех спутников GPS
-#spisok_name_satR = list((obsR['S1'].sv.values))				# формируем список всех спутников GLONASS
+fullTime = min(obsG['S1'].time.values.size, obsR['S1'].time.values.size)  # определение временного диапазона
+
+timeDeltaG = timedelta64(obsG['S1'].time[fullTime - 1].values - obsG['S1'].time[0].values, 's')
+timeDeltaR = timedelta64(obsR['S1'].time[fullTime - 1].values - obsR['S1'].time[0].values, 's')
+
+# spisok_name_satG = list((obsG['S1'].sv.values))				# формируем список всех спутников GPS
+# spisok_name_satR = list((obsR['S1'].sv.values))				# формируем список всех спутников GLONASS
 ng = 0
 nr = 0
-l = 0
-for j in range(b):												# проверка на кол-во спутников с заданным SNR
-	l += 1
-	for k in obsG['S1'][j].values:							
-		if k > int(arg2):
-			ng += 1
-	for m in obsR['S1'][j].values:	
-		if m > int(arg2):
-			nr += 1
-	if ng < int(arg3) and nr < int(arg3):
-		print('detect')
-		print(obsG['S1'][j].time.values)
-		time_jam = obsG['S1'][j].time.values
-		with open('data_jemming_' + arg + '.txt','a') as f:
-			f.write('Drone: ' + arg)
-			f.write(' ')
-			f.write('Time: ' + str(time_jam))
-			f.write('\n')
-		f.close()
-	ng = 0
-	nr = 0
+schet = 0
+for j in range(fullTime):  # проверка на кол-во спутников с заданным SNR
+    schet += 1
+    for k in obsG['S1'][j].values:
+        if k > int(arg2):
+            ng += 1
+    for m in obsR['S1'][j].values:
+        if m > int(arg2):
+            nr += 1
+    if ng < int(arg3) and nr < int(arg3):
+        print('detect')
+        time_jam = str(obsG['S1'][j].time.values)[11:22]
+        print(time_jam)
+        with open(os.path.join(os.getcwd(), folder_name, 'data_jemming_all.csv'), 'a') as f:
+            f.write(time_jam)
+            f.write(' ')
+            f.write(arg)
+            f.write('\n')
+        f.close()
+    ng = 0
+    nr = 0
+with open(os.path.join(os.getcwd(), folder_name, 'data_propuski.csv'), 'a') as f2:
+    propuski = int(str(timeDeltaG*5 - fullTime)[:-8])
+    percent_propuski = round(((propuski) * 100) / fullTime)
+    print(fullTime, propuski, percent_propuski, end='% \n')
+    f2.write(arg + ' ' + str(fullTime) + ' ' + str(propuski) + ' ' + str(percent_propuski) + '%')
+    f2.write('\n')
 
-if l < b:														# проверка на потерянные данные 
-	print('error -', end = ' ')
-	print(b - l, end = ' (')
-	print(round(((b-l)*100)/b), end = ' %)')
-#ax.plot(obsG.time, obsG['S1'], label = spisok_name_satG)		# строим графики SNR для всех спутников GPS
-#ax.legend()	
-#show()
-#ax.plot(obsR.time, obsR['S1'], label = spisok_name_satR)		# строим графики SNR для всех спутников GLONASS
-#ax.legend()									
-#show()
+
+# ax.plot(obsG.time, obsG['S1'], label = spisok_name_satG)		# строим графики SNR для всех спутников GPS
+# ax.legend()
+# show()
+# ax.plot(obsR.time, obsR['S1'], label = spisok_name_satR)		# строим графики SNR для всех спутников GLONASS
+# ax.legend()
+# show()
